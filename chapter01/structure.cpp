e31828aa1a430e7ad67792fd3cafcc3e748ab652
kwon <kkdosk@naver.com>
2020-03-02 11:41:47 +0900

    a

--- chapter01/structure.cpp
+++ chapter01/structure.cpp
@@ -17,8 +17,14 @@ MyStruct getStruct()
 int main(int argc, char *argv[])
 {
   MyStruct ms{42, "Hello"};
-  auto [u, v] = ms;
-  std::cout << u;
+  // auto [u, v] = ms;
+  // std::cout << u << std::endl;
+  // std::cout << &v;
+  // assert(&(((MyStruct *)&u)->s) == &v);
 
-  assert(&((MyStruct *)&u)->s == &v);
+  auto &&[v, n] = std::move(ms);
+  std::cout << "ms.s: " << ms.s << '\n';
+
+  std::string s = std::move(n);
+  std::cout << "ms.s: " << ms.s << '\n';
 }
