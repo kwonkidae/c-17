64c735b67a2c8b260202d9dc22828dd619064cc0
kwon <kkdosk@naver.com>
2020-03-05 15:17:27 +0900

    add

--- chapter01/structure.cpp
+++ chapter01/structure.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <filesystem>
 #include <cstdlib> // for EXIT_FAILURE
-
+#include <array>
 struct MyStruct
 {
   int i = 0;
@@ -13,6 +13,21 @@ MyStruct getStruct()
 {
   return MyStruct{42, "Hello"};
 }
+
+struct B
+{
+  int a = 1;
+  int b = 2;
+};
+
+struct D1 : B
+{
+};
+
+struct D2 : B
+{
+  int c = 3;
+};
 
 int main(int argc, char *argv[])
 {
@@ -27,4 +42,21 @@ int main(int argc, char *argv[])
 
   std::string s = std::move(n);
   std::cout << "ms.s: " << ms.s << '\n';
+
+  auto [z, x] = D1{};
+
+  std::cout << x << " " << z << std::endl;
+
+  int arr[] = {47, 11};
+  auto [x1, y1] = arr;
+
+  std::cout << x1 << " " << y1 << std::endl;
+  {
+    std::array<int, 4> stdarr{1, 2, 3, 4};
+
+    auto &[i, j, k, l] = stdarr;
+    i += 20;
+
+    std::cout << stdarr[0] << std::endl;
+  }
 }
